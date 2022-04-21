dbd665f4c8c47ee4a4ccbfb6966e6c6799eac13b
joshua kilaha <jokilaha191@gmail.com>
2022-04-21 19:52:11 +0300

    User Cell View
    Adding user Cell View

---
+++ View/UserCell.swift
@@ -0,0 +1,62 @@
+//
+//  UserCell.swift
+//  Mock
+//
+//  Created by mroot on 21/04/2022.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    
+    @StateObject var api = ApiCall()
+    
+    @State var showView = false
+    
+    var body: some View {
+        NavigationView {
+            List{
+                ForEach(api.mock, id: \.id) { user in
+                    NavigationLink(destination: EditUser(user: user)) {
+                        VStack(alignment: .leading, spacing: 10){
+                            Text(user.name)
+                                .font(.title2)
+                            if user.status == "Active" {
+                                Text(user.status)
+                                    .foregroundColor(.green)
+                            } else {
+                                Text(user.status)
+                                    .foregroundColor(.red)
+                            }
+                            
+                        }
+                    }
+                } .onDelete(perform: deleteUser)
+            }
+            .navigationTitle("Users")
+        }
+        .navigationViewStyle(.stack)
+        
+        .sheet(isPresented: $showView) {
+            AddUser(user: Mock(id: "", name: "", status: ""))
+        }
+        
+    }
+    //MARK: Detele Item
+    private func deleteUser(indexSet: IndexSet) {
+        
+        let id = indexSet.map {api.mock[$0].id}
+        
+        DispatchQueue.main.async {
+            let item = id
+            api.Mock_DELETE_User1(id: item)
+        }
+    }
+    
+}
+
+struct UserCell_Previews: PreviewProvider {
+    static var previews: some View {
+        UserCell()
+    }
+}
